4389ee996d30ac7df53469b653d76817ce8b049e
nicholas Maluleka <lesetjamaluleka@gmail.com>
2023-12-22 17:24:27 +0200

    minor

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const weatherPromise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=-26.0871908&lon=28.0124675&appid=87a4b4f648814f8709197f45419a77f2')
-const weathers = await weatherPromise.json()
+const weather = await weatherPromise.json()
 
 console.log(weathers);
 
 const template = document.querySelector('#hourly')
 const wrapper = document.createElement('div')
 
-weathers.forEach(weather => {
+weather.forEach(weather => {
     const clone = template.content.cloneNode(true)
     clone.querySelector('h2').textContent = weather
 
