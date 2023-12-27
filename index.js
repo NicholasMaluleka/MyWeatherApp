dde8d0f0270485fdd9a6963c56537926c863ed66
nicholas Maluleka <lesetjamaluleka@gmail.com>
2023-12-27 21:55:49 +0200

    new

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const weatherPromise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=-26.0871908&lon=28.0124675&appid=87a4b4f648814f8709197f45419a77f2')
 const weather = await weatherPromise.json()
 
-console.log(weather);
+console.log(weathers);
 
 const template = document.querySelector('#hourly')
 const wrapper = document.createElement('div')
