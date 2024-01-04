1a2bfeb994f933c8a4f3fdcbcdfbd6dc6187e738
nicholas Maluleka <lesetjamaluleka@gmail.com>
2024-01-04 09:21:44 +0200

    latest

--- index.js
+++ index.js
@@ -5,36 +5,36 @@ const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
 const API_KEY = "87a4b4f648814f8709197f45419a77f2"; // API key for OpenWeatherMap API
-    
-const createWeatherCard = (cityName, weatherItem) => {
-    if(index === 1) { // HTML for the current weather
-        return `<div class="details">
-                    <h2>${cityName} (${weatherItem.dt})</h2>
-                    <h6>Temperature: ${(weatherItem.main.temp)}°C</h6>
-                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
-                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
-                </div>
-                <div class="icon">
-                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png" alt="weather-icon">
-                    <h6>${weatherItem.weather[0].description}</h6>
-                </div>`;
-    } else { // HTML for next 5 days
-        return `<li class="card">
-                    <h3>(${weatherItem.dt})</h3>
-                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png" alt="weather-icon">
-                    <h6>Temp: ${(weatherItem.main.temp)}°C</h6>
-                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
-                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
-                </li>`;
-    }
-    console.log(weatherItem)
-}
 
 // receive city coordinates (latitude, longitude, and name) from the API response
 searchButton.addEventListener("click", getCityCoordinates);
 const getCityCoordinates = () => {
     const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}&unit=metric`;
     const cityName = cityInput.value;
+}
+    
+const weatherCard = (cityName) => {
+    if(index === 1) { // HTML for the current weather
+        return `<div class="details">
+                    <h2>${cityName} (${dt})</h2>
+                    <h6>Temperature: ${(main.temp)}°C</h6>
+                    <h6>Wind: ${wind.speed} M/S</h6>
+                    <h6>Humidity: ${main.humidity}%</h6>
+                </div>
+                <div class="icon">
+                    <img src="https://openweathermap.org/img/wn/${weather[0].icon}.png" alt="weather-icon">
+                    <h6>${weather[0].description}</h6>
+                </div>`;
+    } else { // HTML for next 5 days
+        return `<li class="card">
+                    <h3>(${weatherItem.dt})</h3>
+                    <img src="https://openweathermap.org/img/wn/${weather[0].icon}.png" alt="weather-icon">
+                    <h6>Temp: ${(main.temp)}°C</h6>
+                    <h6>Wind: ${wind.speed} M/S</h6>
+                    <h6>Humidity: ${main.humidity}%</h6>
+                </li>`;
+    }
+    console.log(weatherItem)
 }
 
 locationButton.addEventListener("click", getUserCoordinates);
